11ef4bf78cc0e2b99a48bd63ee017e0e0faf1928
AshSwing <ashswing@email.cn>
2023-02-22 22:06:48 +0800

    add starship and conda config

--- config/mac/install.sh
+++ config/mac/install.sh
@@ -5,3 +5,10 @@ ln -sf ~/AshSwing/config/mac/shrc/zshrc ~/.zshrc
 # Kitty
 rm -rf ~/.config/kitty
 ln -sfn ~/AshSwing/config/mac/kitty ~/.config/kitty
+
+# Starship
+ln -sf ~/AshSwing/config/mac/starship.toml ~/.config/starship.toml
+
+# Conda
+ln -sf ~/AshSwing/config/mac/condarc ~/.condarc
+
